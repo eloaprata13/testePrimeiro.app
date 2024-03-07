d62ad5a44b23c2ef8d30f19de96af014b58ca1e8
eloa.prata13 <eloa.prata@etec.sp.gov.br>
2024-03-07 08:46:43 -0300

    pasta titulo

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Title from './Title/';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Eloá</Text>
+      <Text>dia 07-03-2024</Text>
+      <Title/>
       <StatusBar style="auto" />
     </View>
   );
